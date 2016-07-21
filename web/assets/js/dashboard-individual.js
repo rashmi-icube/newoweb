0bfc87b209b8e33b5599cdae7d75112703164ff0
rashmi-icube <rashmi@i-cube.in>
2016-07-21 16:05:05 +0530

    Fixed the issue for the dashboard filter selection + changing between metrics for appreciation

--- web/assets/js/dashboard-individual.js
+++ web/assets/js/dashboard-individual.js
@@ -496,9 +496,10 @@ function saveRating() {
     jQuery.each($('.star-rating-total'), function(i,v) {
         var empRating = {};
         var empId = $(v).attr('emp_id');
+        var quesId = $(v).attr('ques_id');
         var rating = $(v).text();
         if(rating !== '' && rating > 0) {
-            empRating[empId] = rating;
+            empRating[quesId + '_' + empId] = rating;
             empArr.push(empRating);
         }else {
             jQuery.each(empArr, function(n,o) {
@@ -519,6 +520,7 @@ function clearRatings() {
     jQuery.each($('.star-rating-total'), function(i,v) {
         $(v).text('');
     });
+    jQuery.data(document.body, "emp_rating", []);
 }
 
 function displayRatings(id, score) {
